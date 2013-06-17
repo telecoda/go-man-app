c253001cddbf551e874e3159ca702781f9be733f
telecoda <rob@thebaines.co.uk>
2013-06-17 06:51:11 +0100

    testing 1000 transactions per second

--- controllers/game_controller.go
+++ controllers/game_controller.go
@@ -1,12 +1,22 @@
 package controllers
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/telecoda/go-man/models"
+	"github.com/telecoda/go-man/utils"
 	"net/http"
+	"os"
 )
+
+/* Thanks to the following source for an ASCII version of the game board
+http://4coder.org/c-c-source-code/152/pacman/board.c.html
+
+*/
+
+var defaultBoard [][]byte
 
 func GameList(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
@@ -16,7 +26,7 @@ func GameList(w http.ResponseWriter, r *http.Request) {
 func GameCreate(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 
-	var board = models.NewGameBoard()
+	var board = NewGameBoard()
 
 	board.SaveGameBoard()
 
@@ -27,6 +37,69 @@ func GameCreate(w http.ResponseWriter, r *http.Request) {
 	} else {
 		fmt.Fprint(w, string(bJson))
 	}
+}
+
+func init() {
+	initGameBoard()
+}
+
+func initGameBoard() {
+
+	defaultBoard = make([][]byte, models.BOARD_HEIGHT)
+
+	// read data from maze.dat
+	f, err := os.Open("data/maze.txt")
+	if err != nil {
+		fmt.Printf("error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	r := bufio.NewReader(f)
+
+	var i int = 0
+	for {
+
+		b, err := r.ReadBytes('\n')
+		if err == nil {
+			// parse line
+
+			b = b[:len(b)-1] // remove last new line char from bytes
+			defaultBoard[i] = b
+			fmt.Println(string(defaultBoard[i]))
+			i++
+		} else {
+			break
+		}
+
+	}
+
+}
+
+func NewGameBoard() *models.GameBoard {
+	initGameBoard()
+	gameBoard := new(models.GameBoard)
+
+	id, err := utils.GenUUID()
+	if err != nil {
+		fmt.Println("Error generating guid")
+		return nil
+	}
+	gameBoard.Id = id
+	gameBoard.Name = "Init name"
+	gameBoard.BoardCells = defaultBoard
+
+	// init players
+	gameBoard.MainPlayer = *NewPlayer()
+
+	return gameBoard
+}
+
+func NewPlayer() *models.Player {
+	//return &models.Player{Location: {0, 0}, Id: 1, Type: models.PlayerType.GoMan}
+	player := models.Player{
+		Location: models.Point{models.PLAYER_START_X, models.PLAYER_START_Y},
+		Id:       1,
+	}
+	return &player
 }
 
 func GameById(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +114,8 @@ func GameById(w http.ResponseWriter, r *http.Request) {
 	if board == nil || err != nil {
 		http.NotFound(w, r)
 	}
+
+	fmt.Println("Loaded board", board)
 
 	bJson, err := json.Marshal(board)
 
--- models/game_board.go
+++ models/game_board.go
@@ -1,79 +1,44 @@
 package models
 
-import (
-	"bufio"
-	"fmt"
-	"github.com/telecoda/go-man/utils"
-	"os"
+type Point struct {
+	X, Y int
+}
+
+type PlayerType int
+
+const (
+	GoMan = iota
+	GoGhost
 )
+
+type PlayerState int
+
+const (
+	Alive = iota
+	Dead
+	Spawing
+)
+
+type Player struct {
+	Location Point
+	Id       int
+	Type     PlayerType
+}
 
 type GameBoard struct {
 	Id         string
 	Name       string
+	MainPlayer Player
 	BoardCells [][]byte
 }
 
 const BOARD_WIDTH int = 28
 const BOARD_HEIGHT int = 24
 
-/* Thanks to the following source for an ASCII version of the game board
-http://4coder.org/c-c-source-code/152/pacman/board.c.html
-
-*/
-
-var defaultBoard [][]byte
+const PLAYER_START_X = 13
+const PLAYER_START_Y = 14
 
 var persister = NewFilePersister()
-
-func init() {
-	initGameBoard()
-}
-
-func initGameBoard() {
-
-	defaultBoard = make([][]byte, BOARD_HEIGHT)
-
-	// read data from maze.dat
-	f, err := os.Open("data/maze.txt")
-	if err != nil {
-		fmt.Printf("error opening file: %v\n", err)
-		os.Exit(1)
-	}
-	r := bufio.NewReader(f)
-
-	var i int = 0
-	for {
-
-		b, err := r.ReadBytes('\n')
-		if err == nil {
-			// parse line
-
-			b = b[:len(b)-1] // remove last new line char from bytes
-			defaultBoard[i] = b
-			fmt.Println(string(defaultBoard[i]))
-			i++
-		} else {
-			break
-		}
-
-	}
-
-}
-
-func NewGameBoard() *GameBoard {
-	gameBoard := new(GameBoard)
-
-	id, err := utils.GenUUID()
-	if err != nil {
-		fmt.Println("Error generating guid")
-		return nil
-	}
-	gameBoard.Id = id
-	gameBoard.Name = "Init name"
-	gameBoard.BoardCells = defaultBoard
-
-	return gameBoard
-}
 
 func (model *GameBoard) SaveGameBoard() {
 	persister.Save(model)
