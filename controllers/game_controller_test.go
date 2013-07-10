303239a09726d1588cfd6a846c5a7b73806b25f4
telecoda <rob@thebaines.co.uk>
2013-07-10 07:49:23 +0100

    added some test fixture data, functional tests for controller methods

--- controllers/game_controller_test.go
+++ controllers/game_controller_test.go
@@ -4,20 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/telecoda/go-man/models"
+	"github.com/telecoda/go-man/utils"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func Setup() {
-}
-
-func Teardown(t *testing.T) {
-}
 
 func TestCreateGame(t *testing.T) {
+
+	setup()
+	defer tearDown()
 
 	fmt.Println("TestCreateGame started")
 
@@ -26,7 +24,7 @@ func TestCreateGame(t *testing.T) {
 
 	res, err := http.Post(ts.URL, "application/json", nil)
 	if err != nil {
-		log.Fatal(err)
+		t.Errorf("Error POSTing request to API:",err)
 	}
 	jsonBody, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
@@ -64,3 +62,122 @@ func TestCreateGame(t *testing.T) {
 	fmt.Println("TestCreateGame ended")
 
 }
+
+func TestGameList(t *testing.T) {
+
+	setup()
+	defer tearDown()
+
+	fmt.Println("TestGameList started")
+
+	ts := httptest.NewServer(http.HandlerFunc(GameList))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		t.Errorf("Error GETting request from API:",err)
+	}
+	jsonBody, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Errorf("Failed to read JSON response", err.Error)
+	}
+
+	if jsonBody == nil {
+		t.Errorf("No json returned")
+	}
+
+	var boards []models.GameBoardSummary
+
+	fmt.Println(boards)
+
+	err = json.Unmarshal(jsonBody, &boards)
+
+	if err != nil {
+		t.Errorf("Failed to unmarshal JSON response", err.Error)
+	}
+
+	// check values of boards returned
+
+	if len(boards) != 3 {
+		t.Errorf("There should be 3 boards but we received %d", len(boards))
+	}
+
+	if &boards == nil {
+		t.Errorf("No game boards")
+	}
+
+	fmt.Println("TestGameList ended")
+
+}
+
+
+func TestGameListFilteredByState(t *testing.T) {
+
+	setup()
+	defer tearDown()
+
+	fmt.Println("TestGameListFilteredByState started")
+
+	ts := httptest.NewServer(http.HandlerFunc(GameList))
+	defer ts.Close()
+
+	requestURL := ts.URL + "?state=waiting"
+	res, err := http.Get(requestURL)
+	if err != nil {
+		t.Errorf("Error GETting request from API:",err)
+	}
+	jsonBody, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Errorf("Failed to read JSON response", err.Error)
+	}
+
+	if jsonBody == nil {
+		t.Errorf("No json returned")
+	}
+
+	var boards []models.GameBoardSummary
+
+	fmt.Println(boards)
+
+	err = json.Unmarshal(jsonBody, &boards)
+
+	if err != nil {
+		t.Errorf("Failed to unmarshal JSON response", err.Error)
+	}
+
+	// check values of boards returned
+
+	if len(boards) != 2 {
+		t.Errorf("There should be 2 boards but we received %d", len(boards))
+	}
+
+	if &boards == nil {
+		t.Errorf("No game boards")
+	}
+
+	fmt.Println("TestGameListFilteredByState ended")
+
+}
+/* helper functions */
+
+func setup() {
+	fmt.Println("Test setup")
+	deleteAllGames()
+	copyGameFixtures()
+}
+
+func tearDown() {
+	fmt.Println("Test teardown")
+	deleteAllGames()
+}
+
+func deleteAllGames() {
+	utils.DeleteOldGameBoardFiles()
+}
+
+func copyGameFixtures() {
+	utils.CopyGameDataFixtures("gamedata_fixtures", "gamedata")
+}
+
--- utils/file_management.go
+++ utils/file_management.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-var filePath = GetAbsolutePathOfCurrentPackage("../gamedata/")
+var filePath = GetAbsolutePathOfCurrentPackage(".." + string(os.PathSeparator) + "gamedata" + string(os.PathSeparator))
 
 func DeleteOldGameBoardFiles() {
 	fmt.Println("Deleting old game boards")
@@ -22,10 +22,58 @@ func DeleteOldGameBoardFiles() {
 		name := dir.Name()
 		if !dir.IsDir() && strings.HasSuffix(name, ".json") {
 			fmt.Println("Deleting file:", name)
-			err := os.Remove(filePath + "/" + dir.Name())
+			err := os.Remove(filePath + string(os.PathSeparator) + dir.Name())
 			if err != nil {
 				fmt.Println(err)
 			}
 		}
 	}
+}
+
+func CopyGameDataFixtures(fromDir string, toDir string) {
+	fromPath := GetAbsolutePathOfCurrentPackage(".." + string(os.PathSeparator) + fromDir)
+	toPath := GetAbsolutePathOfCurrentPackage(".." + string(os.PathSeparator) + toDir)
+
+	dirs, err := ioutil.ReadDir(fromPath)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for _, dir := range dirs {
+		name := dir.Name()
+		if !dir.IsDir() && strings.HasSuffix(name, ".json") {
+			fmt.Println("Copying file:", name)
+			oldFile, err := os.Open(fromPath + string(os.PathSeparator) + dir.Name())
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			newFile, err := os.Create(toPath + string(os.PathSeparator) + dir.Name())
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+
+			readBuffer := make([]byte, 5000)
+
+			bytesRead, err := oldFile.Read(readBuffer)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			fmt.Println("Bytes read", bytesRead)
+
+			// truncate buffer to actual size read
+			readBuffer = readBuffer[:bytesRead]
+
+			bytesWritten, err := newFile.Write(readBuffer)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			fmt.Println("Bytes written", bytesWritten)
+		}
+	}
+
 }
